aa08d46e7e1dd5bd65fd05d5cf88a5b9febaa5c3
Setu Shah <setu4993@yahoo.co.in>
2022-10-25 08:39:04 -0700

    fix: Update commit version in config (#6)
    BREAKING CHANGE: Public release
    
    ^ is only to trigger a major version bump.

--- src/transformer_embeddings/__init__.py
+++ src/transformer_embeddings/__init__.py
@@ -3,4 +3,4 @@ from transformer_embeddings.model import TransformerEmbeddings
 from transformer_embeddings.poolers import get_pooler_output, mean_pooling
 
 
-__version__ = "2.1.1"
+__version__ = "2.2.0"
