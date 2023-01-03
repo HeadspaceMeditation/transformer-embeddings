a3791c005c4b8a8f50cda16f5a507555714c16f6
semantic-release <semantic-release>
2023-01-03 11:55:09 -0800

    chore(release): v3.0.3
    Automatically generated by python-semantic-release

--- src/transformer_embeddings/__init__.py
+++ src/transformer_embeddings/__init__.py
@@ -3,4 +3,4 @@ from transformer_embeddings.model import TransformerEmbeddings
 from transformer_embeddings.poolers import get_pooler_output, mean_pooling
 
 
-__version__ = "3.0.2"
+__version__ = "3.0.3"
