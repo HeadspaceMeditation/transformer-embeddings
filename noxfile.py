dc68fb55bd3d335cb6bf54a6dcbb17c304cfee8e
parthjaw <100322869+parthjaw@users.noreply.github.com>
2023-03-27 12:20:30 -0700

    chore(deps): Remove Python 3.7, add numpy explicitly with the updated version (#17)
    Co-authored-by: Setu Shah <setu4993@yahoo.co.in>

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 from nox_poetry import Session, session
 
 
-@session(python=["3.7", "3.8", "3.9", "3.10"])
+@session(python=["3.8", "3.9", "3.10"])
 def tests(session: Session):
     """Run the tests and generate reports."""
     session.run_always("poetry", "install", external=True)
